b12aa5f2d2aae45c1713faae16af07766ef3359a
yudhistirageraldo <yudhistirageraldo@gmail.com>
2026-04-28 19:31:16 +0700

    menambahkan void

--- FriendFunctionSatu.cpp
+++ FriendFunctionSatu.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
 #include <string>
 using namespace std;
+class mahasiswa
+{
+private:
+    string nama;
+public:
+    friend void setNama(mahasiswa &a, string);
+};
+
+void setNama(mahasiswa &a, string b)
+{
+    a.nama = b;
+    cout << a.nama;
+}
+
